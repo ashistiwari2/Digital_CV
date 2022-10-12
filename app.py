dc449b6cf09d5daeb7e750f1d168d728a4fa5b9a
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2022-10-12 11:36:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,14 @@ W_EMAIL='Ashis.Tiwari@in.ey.com'
 C_EMAIL="ashis_201800333@smit.smu.edu.in"
 SOCIAL_MEDIA = {
     "🎫Instagram": "https://www.instagram.com/ashistiwari2",
-    "🥇LinkedIn": "https://www.linkedin.com/in/ashis-tiwari-9aa527213/",
+    "⬜🟦🟦🟦🟦🟦⬜
+🟦🟦🟦🟦🟦🟦🟦
+🟦🟦🟦🟦🟦🟦🟦
+🟦🟩🟦⬜🟦🟩🟦
+🟦🟩🟦🟩🟦⬜🟦
+🟦🟩🟦🟦🟦⬜🟦
+⬜🟦🟦🟦🟦🟦🟩
+LinkedIn": "https://www.linkedin.com/in/ashis-tiwari-9aa527213/",
     "⚽GitHub": "https://github.com/ashistiwari2",
     "🏉Twitter": "https://twitter.com/Ashis_Tiwari_2",
 }
