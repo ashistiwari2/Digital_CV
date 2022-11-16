3baa120b26289c9dc692f8a6fcd5091ab12db099
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2022-11-16 15:23:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ st.write("----------------------------------------------------------------------
 components.html(
     """
     <script src="https://widgets.coingecko.com/coingecko-coin-heatmap-widget.js"></script>
-<coingecko-coin-heatmap-widget  height="400" locale="en" top="500"></coingecko-coin-heatmap-widget>
+<coingecko-coin-heatmap-widget  height="400" locale="en" top="100"></coingecko-coin-heatmap-widget>
 """,
     height=600,)
 # st.write("-------------------------------------------------------------------------------------------")
