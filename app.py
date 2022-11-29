6f8066323d2dbf0e6d319b6895f0e251f99beb22
ashistiwari2 <66716885+ashistiwari2@users.noreply.github.com>
2022-11-29 12:27:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -300,6 +300,8 @@ components.html(
       id="aisensy-wa-widget"
       widget-id="gLwq0K"
     >
+    <br>
+    <br>
     </script>
     <img src="https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fapi.whatsapp.com%2Fsend%3Fphone%3D7432047169%26text%3DHi%2520%252CThank%2520You%2520For%2520messaging%2520Me&chs=180x180&choe=UTF-8&chld=L|2" alt="qr code"><a href="www.qr-code-generator.com/" border="0" style="cursor:default" rel="nofollow"></a>
     <script src="https://apps.elfsight.com/p/platform.js" defer></script>
